44a8db920efa6ddd2bfe07177ac512204cf1ad5c
Eugenia Bonocore <eugeniabonocore@gmail.com>
2024-06-13 10:48:13 +0100

    adjust tags to append to the default class

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -4,11 +4,11 @@
     $defaultClass = "bg-white/10 hover:bg-white/25 font-bold rounded-xl transition-colors duration-300";
 
     if ($size === "base") {
-        $defaultClass = " px-5 py-1 text-sm";
+        $defaultClass .= " px-5 py-1 text-sm";
     }
 
     if ($size === "small") {
-        $defaultClass = " px-3 py-1 text-2xs";
+        $defaultClass .= " px-3 py-1 text-2xs";
     }
 @endphp
 
